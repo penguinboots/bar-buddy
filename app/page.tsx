09d10a97ece0033eaad0ebd0452d688ae9ca1de2
penguinboots <sabrina.ynw@gmail.com>
2023-09-20 13:19:46 -0700

    route testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+import { getUserData } from "@/prisma/helpers/getUserData";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return (<div>Home</div>);
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const initialize = async () => {
+      let userData = await getUserData();
+      setUser(userData.name);
+    }
+    initialize()
+  }, [user])
+  return <div>Hello {user}</div>;
 }
